1b3290278d552e201de657227ef567ee0d8f270d
Dieter Detlef <j.pfeiffer93@gmx.de>
2015-10-24 23:07:45 +0200

    reading the live feature

--- src/header/gt.cpp
+++ src/header/gt.cpp
@@ -9,6 +9,7 @@
 //config class
 gt::config::config()//config constructor
 {
+	live=false;
 	gt::config::readconfig();
 }
 
@@ -18,38 +19,47 @@ void gt::config::readconfig() //reading the configfile
 	std::ifstream config_file;
 	config_file.open("data/config");
 	if(config_file.is_open()) //when file is readable
+	{
+		std::string in_line; //config info (1 line)
+		//read until end of file
+		while(config_file.eof()==false)
 		{
-			std::string in_line; //config info (1 line)
-			//read until end of file
-			while(config_file.eof()==false)
+			getline(config_file,in_line);
+			//checking for keywords
+			//resx
+			if(in_line.find("resx=")!=std::string::npos)
 			{
-				getline(config_file,in_line);
-				std::cout << in_line << "\n";
-				//checking for keywords
-				//resx
-				if(in_line.find("resx=")!=std::string::npos)
+				in_line.erase(0,sizeof("resx=")-1);
+				resx=atoi(in_line.c_str());
+			}
+			//resy
+			if(in_line.find("resy=")!=std::string::npos)
+			{
+				in_line.erase(0,sizeof("resy=")-1);
+				resy=atoi(in_line.c_str());
+			}
+			//seed
+			if(in_line.find("seed=")!=std::string::npos)
+			{
+				in_line.erase(0,sizeof("seed=")-1);
+				seed=atoi(in_line.c_str());
+			}
+			//live
+			if(in_line.find("live=")!=std::string::npos)
+			{
+				in_line.erase(0,sizeof("live=")-1);
+				if(in_line=="1")//if zero it stays disabled
 				{
-					in_line.erase(0,sizeof("resx=")-1);
-					resx=atoi(in_line.c_str());
-				}
-				//resy
-				if(in_line.find("resy=")!=std::string::npos)
-				{
-					in_line.erase(0,sizeof("resy=")-1);
-					resy=atoi(in_line.c_str());
-				}
-				//seed
-				if(in_line.find("seed=")!=std::string::npos)
-				{
-					in_line.erase(0,sizeof("seed=")-1);
-					seed=atoi(in_line.c_str());
-				}
+					live=true;
+					std::cout << "LIVE enabled\n";
+				}			
 			}
 		}
+	}
 	else //if an error while reading occurs
-		{
-			std::cout << "error while reading data/config\n";
-		}
+	{
+		std::cout << "error while reading data/config\n";
+	}
 	
 	config_file.close();
 }
@@ -67,4 +77,9 @@ int gt::config::getresy()
 long int gt::config::getseed()
 {
 	return seed;
+}
+
+bool gt::config::getlive()
+{
+	return live;
 }
--- src/header/gt.hpp
+++ src/header/gt.hpp
@@ -3,19 +3,32 @@
 
 namespace gt
 {
-        class config
-        {
-                private:
-                       	int resx;
-                       	int resy;
-                       	long int seed;
-                public:
-                        config(); //constructor
-                	void readconfig(); //read the config file data/config
-                	int getresx(); //getting the x-resolution
-                	int getresy(); //getting the y-resolution
-                	long int getseed(); //getting the seed
-        };
+	//configuration class
+    class config
+    {
+        private:
+            int resx;
+           	int resy;
+			bool live;
+           	long int seed;
+        public:
+            config(); //constructor
+           	void readconfig(); //read the config file data/config
+           	int getresx(); //getting the x-resolution
+           	int getresy(); //getting the y-resolution
+           	long int getseed(); //getting the seed
+			bool getlive(); //getting the live-value
+    };
+
+	//GUI class
+	class gui
+	{
+		private:
+            bool render_interface; //the state of the renderinterface
+		public:
+			gui(); //constructor
+			void gui_init(); //create the GUI
+	};
 }
 
 
